541106ff1b58127511f70193ead5b7d07234954e
shreedhara <shreedhara@sigmoidanalytics.com>
2021-11-23 11:07:02 +0530

    Updated push.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$GCR_CRED" > abc.json
+cat "$GCR_CRED" > abc.json
 docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
 gcloud container clusters get-credentials cluster-1 --zone us-central1-c
 kubectl create -f jenkins/deploy/deployment.yml
