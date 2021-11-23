f72e0b3e302148ca0054442f90cfad49fc263ef8
shreedhara <shreedhara@sigmoidanalytics.com>
2021-11-23 07:41:16 +0530

    deploy.sh file updated

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,6 +3,6 @@
 echo "$GCR_CRED" > abc.json
 docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
 gcloud container clusters get-credentials cluster-1 --zone us-central1-c
-kubectl create -f deployment.yml
+kubectl create -f jenkins/deploy/deployment.yml
 docker logout https://gcr.io
 
