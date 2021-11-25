a3897f9a862e7a42bc2d32052e8737706a1c226d
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-25 13:10:47 +0530

    Update list.sh

--- jenkins/list/list.sh
+++ jenkins/list/list.sh
@@ -7,18 +7,21 @@ echo "*******************************"
 
 cat "$GCR_CRED" > abc.json
 docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
-
+gcloud container clusters get-credentials cluster-1 --zone us-central1-c
 gcloud container images list --repository=gcr.io/global-memento-332706
 
 echo "*******************************"
 echo "** Listing Pods in cluster ****"
 echo "*******************************"
 
+kubectl get po
 
 
 echo "***********************************"
 echo "** Listing Deployment in cluster **"
 echo "***********************************"
+
+kubectl get deploy
 
 docker logout https://gcr.io
 
