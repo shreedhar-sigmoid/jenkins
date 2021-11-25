0ebd82588cd24a8499e71119c04203f0d2cbcc7f
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-25 13:09:04 +0530

    Update list.sh

--- jenkins/list/list.sh
+++ jenkins/list/list.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
+echo "*******************************"
+echo "** Listing Image in registry **"
+echo "*******************************"
+
+
+cat "$GCR_CRED" > abc.json
+docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
+
+gcloud container images list --repository=gcr.io/global-memento-332706
+
+echo "*******************************"
+echo "** Listing Pods in cluster ****"
+echo "*******************************"
+
+
+
+echo "***********************************"
+echo "** Listing Deployment in cluster **"
+echo "***********************************"
+
+docker logout https://gcr.io
+
 
