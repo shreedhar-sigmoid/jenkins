fefe948ddc01a019bec32111d3e3ce39f61e1341
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-25 12:06:37 +0530

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo "********************"
+echo "** Building Image **"
+echo "********************"
+
 #copy python file
 cp python/sample.py jenkins/build/
 
 #Build images
 docker-compose -f jenkins/build/docker-compose.yml build
+
+
+echo "**********************"
+echo "** Image build done **"
+echo "**********************"
+
+
+
+
