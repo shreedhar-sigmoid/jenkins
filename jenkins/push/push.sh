b473f12bbebc5a6f7d4780c3b68df6e053431f73
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-25 12:20:29 +0530

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -4,7 +4,10 @@ echo "*******************************"
 echo "** Pushing Image to registry **"
 echo "*******************************"
 
+echo "******* Tagging Image *********"
+
 docker tag jenkins-assignment:v1 gcr.io/global-memento-332706/jenkins-assignment:v1
+
 cat "$GCR_CRED" > abc.json
 docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
 docker push gcr.io/global-memento-332706/jenkins-assignment:v1
