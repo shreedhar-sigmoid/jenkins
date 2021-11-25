2d6d7c7505b80a8689202217085a1627b4af95de
shreedhar-sigmoid <91470568+shreedhar-sigmoid@users.noreply.github.com>
2021-11-25 12:10:37 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "**************************"
+echo "** Deploying to cluster **"
+echo "**************************"
+
 cat "$GCR_CRED" > abc.json
 docker login -u _json_key -p "$(cat abc.json)" https://gcr.io
 gcloud container clusters get-credentials cluster-1 --zone us-central1-c
 kubectl create -f jenkins/deploy/deployment.yml
 docker logout https://gcr.io
+
+echo "***************************"
+echo "** successfully deployed **"
+echo "***************************"
 
